3ce0ddf44e5bb8d56f4851e6cd4eba3e2c10dbce
milshael7 <wdelvoix23@gmail.com>
2026-02-02 23:09:53 -0600

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-import Trading from "./pages/Trading.jsx";
+import TradingLayout from "./pages/trading/TradingLayout.jsx";
 
 export default function App() {
-  return (
-    <div className="container">
-      <Trading />
-    </div>
-  );
+  return <TradingLayout />;
 }
