ed300482cb641341eb5e832b382850abe401b13b
milshael7 <wdelvoix23@gmail.com>
2026-02-03 20:47:26 -0600

    Delete AutoShieldTech 2/backend/src directory

--- AutoShieldTech 2/backend/src/auth/jwt.js
+++
@@ -1,4 +0,0 @@
-const jwt = require('jsonwebtoken');
-const sign = (payload, secret, expiresIn='7d') => jwt.sign(payload, secret, { expiresIn });
-const verify = (token, secret) => jwt.verify(token, secret);
-module.exports = { sign, verify };
