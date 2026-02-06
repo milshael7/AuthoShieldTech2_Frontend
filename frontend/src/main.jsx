d7b19f20d8ebb4ea03ef6332911d15081885ce29
milshael7 <wdelvoix23@gmail.com>
2026-02-05 18:18:03 -0600

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+// Global styles
+import "./styles/main.css";
+import "./styles/layout.css";
+
 const rootEl = document.getElementById("root");
 if (!rootEl) throw new Error("Root element #root not found");
 
