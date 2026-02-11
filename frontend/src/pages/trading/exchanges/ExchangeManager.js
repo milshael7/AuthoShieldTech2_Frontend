dbec85b6af32f3492ad02e699470a6258e030330
milshael7 <wdelvoix23@gmail.com>
2026-02-11 15:18:32 -0600

    Update ExchangeManager.js

--- frontend/src/pages/trading/exchanges/ExchangeManager.js
+++ frontend/src/pages/trading/exchanges/ExchangeManager.js
@@ -1,1 +1,31 @@
+import { PaperExchange } from "./PaperExchange";
+import { CoinbaseExchange } from "./CoinbaseExchange";
+import { KrakenExchange } from "./KrakenExchange";
 
+export class ExchangeManager {
+  constructor({ mode = "paper" }) {
+    this.mode = mode;
+    this.exchanges = {
+      paper: new PaperExchange(),
+      coinbase: new CoinbaseExchange(),
+      kraken: new KrakenExchange(),
+    };
+  }
+
+  getExchange(name) {
+    return this.exchanges[name];
+  }
+
+  async executeOrder({
+    exchange,
+    symbol,
+    side,
+    size,
+  }) {
+    const ex = this.getExchange(
+      this.mode === "paper" ? "paper" : exchange
+    );
+
+    return ex.placeOrder({ symbol, side, size });
+  }
+}
