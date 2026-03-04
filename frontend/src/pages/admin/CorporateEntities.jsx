392f50274d4fcb74e6006cd7535773463c39e1f7
milshael7 <wdelvoix23@gmail.com>
2026-03-04 09:22:57 -0500

    Add CorporateEntities component for admin page

---
+++ frontend/src/pages/admin/CorporateEntities.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function CorporateEntities() {
+
+  return (
+    <div style={{ padding: 24 }}>
+      <h2>Corporate Entities</h2>
+
+      <p>
+        Manage enterprise companies and tenant structure.
+      </p>
+    </div>
+  );
+
+}
